31f584128ca8891d6ea3cc7a2051d900c99567dc
vibiht <89259820+vibiht@users.noreply.github.com>
2021-09-13 15:27:33 -0400

    Update monetraMerchantSetup.py

--- monetraMerchantSetup.py
+++ monetraMerchantSetup.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+import configparser as cp
 
 class merchant_account():
     def __init__(self, user:str,merchNum:str):
@@ -14,4 +14,22 @@ class merchEMV(merchant_account):
         self.creditECR = creditECR
 
 def main():
-    f - open("")
+    config = cp.ConfigParser()
+    config.read(r"C:\Users\VTHIRU\Repositories\precise-scripts\monetraMerchantSetup.ini")
+    host=config.get("INPUT","host")
+    excelPath=config.get("INPUT","filepath")
+    sheetName=config.get("INPUT","sheetname")
+    rowStart=config.getint("INPUT", "rowStart")
+    rowCount=config.getint("INPUT", "rowCount")
+    #Parse Merchant #, Monetra User, iECR, cECR
+    sheet = pd.read_excel(excelPath,sheetName,usecols="H,M,R,S",dtype={"H":str,"M":str,"R":str,"S":str},skiprows=rowStart,nrows=rowCount)
+
+    for index,row in sheet.iterrows():
+        excelRow = merchEMV(row[1],row[0],row[2],row[3])
+        print (excelRow.user)
+        print (excelRow.merchNum)
+        print (excelRow.interacECR)
+        print (excelRow.creditECR)
+
+if __name__ == "__main__":
+    main()
