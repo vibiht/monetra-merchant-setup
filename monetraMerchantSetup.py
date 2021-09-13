6e13a050d85fb48bc8904c75848714750f484f1d
vibiht <89259820+vibiht@users.noreply.github.com>
2021-09-13 10:59:40 -0400

    Create monetraMerchantSetup

---
+++ monetraMerchantSetup.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+class merchant_account():
+    def __init__(self, user:str,merchNum:str):
+        self.user = user
+        self.merchNum = merchNum
+    
+class merchEMV(merchant_account):
+    def __init__(self, user:str,merchNum:str, interacECR: str, creditECR: str):
+        super().__init__(user, merchNum)
+        self.interacECR = interacECR
+        self.creditECR = creditECR
