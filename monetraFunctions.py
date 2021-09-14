9f0263cd0c0416600d973b9de742029dccd6a76d
vibiht <89259820+vibiht@users.noreply.github.com>
2021-09-14 14:29:10 -0400

    Modified payload to include both debit and credit merchant creation in one transaction

--- monetraFunctions.py
+++ monetraFunctions.py
@@ -1,8 +1,52 @@
-def addMerchantUser():
+import json
+
+def addMerchantAccountUser(user:str, merchNum:str, interacECR:str, creditECR:str):
     #Monetra_AIP_v8.17.0.pdf Section 4.3.1
+    if accountHierarchy == 0:
+        cardTypes="INTERAC"
+        entryModes="INTERAC_CONTACT|INTERAC_CONTACTLESS"
+    else:
+        cardTypes="VISA+MC+AMEX+DISC+JCB"
+        entryModes="VISA_CONTACT|VISA_CONTACTLESS|MC_CONTACT|MC_CONTACTLESS|AMEX_CONTACT|AMEX_CONTACTLESS|DISC_CONTACT|DISC_CONTACTLESS|JCB_CONTACT|JCB_CONTACTLESS"
+
+    payload = {
+        "debitMerch": {
+            "action":"adduser",
+            "user":user,
+            "pwd":user+"*3984052",
+            "sub":0,
+            "proc":"spdh",
+            "indcode":"RS",
+            "cardtypes":"INTERAC",
+            "mode":"BOTH",
+            "email":"system.monitoring@preciserd.ca",
+            "emv_entrymodes":"INTERAC_CONTACT|INTERAC_CONTACTLESS",
+            "MERCHID":"00"+merchNum,
+            "TERMID":interacECR,
+            "countrycode":"124",
+            "currencycode":"124"}, 
+        "creditMerch": {
+            "action":"adduser",
+            "user":user,
+            "pwd":user+"*3984052",
+            "sub":1,
+            "proc":"spdh",
+            "indcode":"RS",
+            "cardtypes":"VISA+MC+AMEX+DISC+JCB",
+            "mode":"BOTH",
+            "email":"system.monitoring@preciserd.ca",
+            "emv_entrymodes":"VISA_CONTACT|VISA_CONTACTLESS|MC_CONTACT|MC_CONTACTLESS|AMEX_CONTACT|AMEX_CONTACTLESS|DISC_CONTACT|DISC_CONTACTLESS|JCB_CONTACT|JCB_CONTACTLESS",
+            "MERCHID":"00"+merchNum,
+            "TERMID":creditECR,
+            "countrycode":"124",
+            "currencycode":"124"}, 
+            }
+    return (json.dumps(payload))
 
 def addMerchantSubUser():
     #Monetra_AIP_v8.17.0.pdf Section 5.1.1
+    pass
 
 def addMerchantCronTask():
-    #Monetra_AIP_v8.17.0.pdf Section 5.10
+    #Monetra_AIP_v8.17.0.pdf Section 5.10 
+    pass
--- monetraMerchantSetup.py
+++ monetraMerchantSetup.py
@@ -1,23 +1,25 @@
-import numpy as np
-import pandas as pd
-import configparser as cp
-import urllib.request as urlReq
+from datetime import time
+import numpy
+import pandas
+import configparser
+import urllib.request
 import json
 import monetraFunctions
 
 class merchant_account():
-    def __init__(self, user:str,merchNum:str):
+    def __init__(self, user:str,merchNum:str,settleTime:time):
         self.user = user
         self.merchNum = merchNum
+        self.settleTime = settleTime
     
 class merchEMV(merchant_account):
-    def __init__(self, user:str,merchNum:str, interacECR: str, creditECR: str):
-        super().__init__(user, merchNum)
+    def __init__(self, user:str,merchNum:str, settleTime:time, interacECR: str, creditECR: str):
+        super().__init__(user, merchNum,settleTime)
         self.interacECR = interacECR
         self.creditECR = creditECR
 
 def main():
-    config = cp.ConfigParser()
+    config = configparser.ConfigParser()
     #Config.read(r"C:\Users\VTHIRU\Repositories\precise-scripts\monetraMerchantSetup.ini")
     config.read(r"monetraMerchantSetup.ini")
     host=config.get("INPUT","host")
@@ -26,14 +28,15 @@ def main():
     rowStart=config.getint("INPUT", "rowStart")
     rowCount=config.getint("INPUT", "rowCount")
     #Parse Merchant #, Monetra User, iECR, cECR
-    sheet = pd.read_excel(excelPath,sheetName,usecols="H,M,R,S",dtype={"H":str,"M":str,"R":str,"S":str},skiprows=rowStart,nrows=rowCount)
+    sheet = pandas.read_excel(excelPath,sheetName,usecols="H,M,R,S,X",dtype={"H":str,"M":str,"R":str,"S":str,"X":time},skiprows=rowStart,nrows=rowCount)
 
     for index,row in sheet.iterrows():
-        excelRow = merchEMV(row[1],row[0],row[2],row[3])
+        excelRow = merchEMV(row[1],row[0],row[4],row[2],row[3])
         print (excelRow.user)
         print (excelRow.merchNum)
         print (excelRow.interacECR)
         print (excelRow.creditECR)
+        print (excelRow.settleTime)
 
 if __name__ == "__main__":
     main()
