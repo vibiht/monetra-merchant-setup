2d6e7c6e9000711bac78629ef1522418affce2c2
vibiht <89259820+vibiht@users.noreply.github.com>
2021-09-14 15:40:13 -0400

    Added remaining Monetra functions and modified test .ini and .xlsx

--- monetraFunctions.py
+++ monetraFunctions.py
@@ -1,52 +1,82 @@
+from datetime import time
 import json
 
 def addMerchantAccountUser(user:str, merchNum:str, interacECR:str, creditECR:str):
     #Monetra_AIP_v8.17.0.pdf Section 4.3.1
-    if accountHierarchy == 0:
-        cardTypes="INTERAC"
-        entryModes="INTERAC_CONTACT|INTERAC_CONTACTLESS"
-    else:
-        cardTypes="VISA+MC+AMEX+DISC+JCB"
-        entryModes="VISA_CONTACT|VISA_CONTACTLESS|MC_CONTACT|MC_CONTACTLESS|AMEX_CONTACT|AMEX_CONTACTLESS|DISC_CONTACT|DISC_CONTACTLESS|JCB_CONTACT|JCB_CONTACTLESS"
-
     payload = {
-        "debitMerch": {
-            "action":"adduser",
-            "user":user,
-            "pwd":user+"*3984052",
-            "sub":0,
-            "proc":"spdh",
-            "indcode":"RS",
-            "cardtypes":"INTERAC",
-            "mode":"BOTH",
-            "email":"system.monitoring@preciserd.ca",
-            "emv_entrymodes":"INTERAC_CONTACT|INTERAC_CONTACTLESS",
-            "MERCHID":"00"+merchNum,
-            "TERMID":interacECR,
-            "countrycode":"124",
-            "currencycode":"124"}, 
-        "creditMerch": {
-            "action":"adduser",
-            "user":user,
-            "pwd":user+"*3984052",
-            "sub":1,
-            "proc":"spdh",
-            "indcode":"RS",
-            "cardtypes":"VISA+MC+AMEX+DISC+JCB",
-            "mode":"BOTH",
-            "email":"system.monitoring@preciserd.ca",
-            "emv_entrymodes":"VISA_CONTACT|VISA_CONTACTLESS|MC_CONTACT|MC_CONTACTLESS|AMEX_CONTACT|AMEX_CONTACTLESS|DISC_CONTACT|DISC_CONTACTLESS|JCB_CONTACT|JCB_CONTACTLESS",
-            "MERCHID":"00"+merchNum,
-            "TERMID":creditECR,
-            "countrycode":"124",
-            "currencycode":"124"}, 
-            }
+        "MonetraTrans": {
+            "debitMerch": {
+                "action":"adduser",
+                "username":"MADMIN",
+                "password":"precise*3984052",
+                "user":user,
+                "pwd":user+"*3984052",
+                "sub":0,
+                "proc":"spdh",
+                "indcode":"RS",
+                "cardtypes":"INTERAC",
+                "mode":"BOTH",
+                "email":"system.monitoring@preciserd.ca",
+                "emv_entrymodes":"INTERAC_CONTACT|INTERAC_CONTACTLESS",
+                "emv_termcaps":"OFFLINEPIN",
+                "MERCHID":"00"+str(merchNum),
+                "TERMID":interacECR,
+                "countrycode":"124",
+                "currencycode":"124"
+            }, 
+            "creditMerch": {
+                "action":"adduser",
+                "username":"MADMIN",
+                "password":"precise*3984052",
+                "user":user,
+                "pwd":user+"*3984052",
+                "sub":1,
+                "proc":"spdh",
+                "indcode":"RS",
+                "cardtypes":"VISA+MC+AMEX+DISC+JCB",
+                "mode":"BOTH",
+                "email":"system.monitoring@preciserd.ca",
+                "emv_entrymodes":"VISA_CONTACT|VISA_CONTACTLESS|MC_CONTACT|MC_CONTACTLESS|AMEX_CONTACT|AMEX_CONTACTLESS|DISC_CONTACT|DISC_CONTACTLESS|JCB_CONTACT|JCB_CONTACTLESS",
+                "emv_termcaps":"OFFLINEPIN",
+                "MERCHID":"00"+str(merchNum),
+                "TERMID":creditECR,
+                "countrycode":"124",
+                "currencycode":"124"
+            }, 
+        }
+    }
     return (json.dumps(payload))
 
-def addMerchantSubUser():
+def addMerchantSubUser(user:str):
     #Monetra_AIP_v8.17.0.pdf Section 5.1.1
-    pass
+    payload = {
+        "MonetraTrans": {
+            "subUser": {
+                "action":"admin",
+                "admin":"subuseradd",
+                "username":user,
+                "password":user+"*3984052",
+                "admintypes":"GETPERMS|MERCHINFO|RECURRINGADD|IMAGEADD",
+                "trantypes":"SALE|REVERSAL|VOID|TOREVERSAL|CARDTYPE|TERMLOAD|INTERACMAC|EMVCOMPLETE|ADMIN|CHKPWD",
+                "userflags":"obscure|unattended"
+            } 
+        }
+    }
+    return (json.dumps(payload))
 
-def addMerchantCronTask():
+def addMerchantCronTask(user:str, settleTime:str):
     #Monetra_AIP_v8.17.0.pdf Section 5.10 
-    pass
+    payload = {
+        "MonetraTrans": {
+            "cronTask": {
+                "username":user,
+                "password":user+"*3984052",
+                "action":"admin",
+                "admin":"cron",
+                "cron":"add",
+                "cron_date":settleTime+"|*",
+                "cron_task":"settle"
+            } 
+        }
+    }
+    return (json.dumps(payload))
--- monetraMerchantSetup.py
+++ monetraMerchantSetup.py
@@ -4,10 +4,10 @@ import pandas
 import configparser
 import urllib.request
 import json
-import monetraFunctions
+from monetraFunctions import *
 
 class merchant_account():
-    def __init__(self, user:str,merchNum:str,settleTime:time):
+    def __init__(self, user:str,merchNum:str,settleTime:str):
         self.user = user
         self.merchNum = merchNum
         self.settleTime = settleTime
@@ -20,23 +20,18 @@ class merchEMV(merchant_account):
 
 def main():
     config = configparser.ConfigParser()
-    #Config.read(r"C:\Users\VTHIRU\Repositories\precise-scripts\monetraMerchantSetup.ini")
     config.read(r"monetraMerchantSetup.ini")
     host=config.get("INPUT","host")
     excelPath=config.get("INPUT","filepath")
     sheetName=config.get("INPUT","sheetname")
     rowStart=config.getint("INPUT", "rowStart")
     rowCount=config.getint("INPUT", "rowCount")
-    #Parse Merchant #, Monetra User, iECR, cECR
+    #Parse Merchant #, Monetra User, iECR, cECR, Settlement Time
     sheet = pandas.read_excel(excelPath,sheetName,usecols="H,M,R,S,X",dtype={"H":str,"M":str,"R":str,"S":str,"X":time},skiprows=rowStart,nrows=rowCount)
 
     for index,row in sheet.iterrows():
         excelRow = merchEMV(row[1],row[0],row[4],row[2],row[3])
-        print (excelRow.user)
-        print (excelRow.merchNum)
-        print (excelRow.interacECR)
-        print (excelRow.creditECR)
-        print (excelRow.settleTime)
+        print (addMerchantAccountUser(excelRow.user, excelRow.merchNum,excelRow.interacECR,excelRow.creditECR))
 
 if __name__ == "__main__":
     main()
