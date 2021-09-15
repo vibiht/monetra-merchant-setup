86bf052d9bee62a3eb5ee35e5f6e6fb05354332d
vibiht <89259820+vibiht@users.noreply.github.com>
2021-09-15 15:42:12 -0400

    Rewrote merchantAccountUser func to allow account tier argu
    func must be called twice for debitMerch and creditMerch

--- monetraFunctions.py
+++ monetraFunctions.py
@@ -4,61 +4,36 @@ import json
 def addMerchantAccountUser(
     user:str,
     merchNum:str,
-    interacECR:str, 
-    creditECR:str
+    ecr:str,
+    accountTier:int
 ):
     #Monetra_AIP_v8.17.0.pdf Section 4.3.1
-    payload = {
-        "MonetraTrans": {
-            "debitMerch": {
-                "action":"adduser",
-                "username":"MADMIN",
-                "password":"precise*3984052",
-                "user":user,
-                "pwd":user+"*3984052",
-                "sub":0,
-                "proc":"spdh",
-                "indcode":"RS",
-                "cardtypes":"INTERAC",
-                "mode":"BOTH",
-                "email":"system.monitoring@preciserd.ca",
-                "emv_entrymodes":"INTERAC_CONTACT|INTERAC_CONTACTLESS",
-                "emv_termcaps":"OFFLINEPIN",
-                "MERCHID":"00"+str(merchNum),
-                "TERMID":interacECR,
-                "countrycode":"124",
-                "currencycode":"124"
-            }, 
-            "creditMerch": {
-                "action":"adduser",
-                "username":"MADMIN",
-                "password":"precise*3984052",
-                "user":user,
-                "pwd":user+"*3984052",
-                "sub":1,
-                "proc":"spdh",
-                "indcode":"RS",
-                "cardtypes":"VISA+MC+AMEX+DISC+JCB",
-                "mode":"BOTH",
-                "email":"system.monitoring@preciserd.ca",
-                "emv_entrymodes":"VISA_CONTACT|VISA_CONTACTLESS|MC_CONTACT|MC_CONTACTLESS|AMEX_CONTACT|AMEX_CONTACTLESS|DISC_CONTACT|DISC_CONTACTLESS|JCB_CONTACT|JCB_CONTACTLESS",
-                "emv_termcaps":"OFFLINEPIN",
-                "MERCHID":"00"+str(merchNum),
-                "TERMID":creditECR,
-                "countrycode":"124",
-                "currencycode":"124"
-            }, 
-        }
+    payload= {
+        "action":"adduser",
+        "username":"MADMIN",
+        "password":"precise*3984052",
+        "user":user,
+        "pwd":user+"*3984052",
+        "sub":accountTier,
+        "proc":"spdh",
+        "indcode":"RS",
+        "cardtypes":"INTERAC",
+        "mode":"BOTH",
+        "email":"system.monitoring@preciserd.ca",
+        "emv_entrymodes":"INTERAC_CONTACT|INTERAC_CONTACTLESS",
+        "emv_termcaps":"OFFLINEPIN",
+        "MERCHID":"00"+str(merchNum),
+        "TERMID":ecr,
+        "countrycode":"124",
+        "currencycode":"124"
     }
-    return (json.dumps(payload))
+    return (payload)
 
 def addMerchantSubUser(
     user:str
 ):
     #Monetra_AIP_v8.17.0.pdf Section 5.1.1
-    payload = {
-        "MonetraTrans": {
-            "subUser": {
+    payload ={
                 "action":"admin",
                 "admin":"subuseradd",
                 "username":user,
@@ -69,9 +44,7 @@ def addMerchantSubUser(
                 "trantypes":"SALE|REVERSAL|VOID|TOREVERSAL|CARDTYPE|TERMLOAD|INTERACMAC|EMVCOMPLETE|ADMIN|CHKPWD",
                 "userflags":"obscure|unattended"
             } 
-        }
-    }
-    return (json.dumps(payload))
+    return (payload)
 
 def addMerchantCronTask(
     user:str, 
@@ -79,8 +52,6 @@ def addMerchantCronTask(
 ):
     #Monetra_AIP_v8.17.0.pdf Section 5.10 
     payload = {
-        "MonetraTrans": {
-            "cronTask": {
                 "username":user,
                 "password":user+"*3984052",
                 "user":user,
@@ -91,17 +62,13 @@ def addMerchantCronTask(
                 "cron_date":''.join(char for char in str(settleTime) if char.isalnum())[0:4]+"|*", #Removing special characters from time
                 "cron_task":"settle"
             } 
-        }
-    }
+
+    return (payload)
+
+def payloadGenerator(
+    *args
+):
+    payload={"MonetraTrans":{}}
+    for i,element in enumerate(args):
+        payload["MonetraTrans"][i]=element
     return (json.dumps(payload))
-
-#Come back and finish this later
-# def payloadGenerator(
-#     *elements
-# ):
-#     for each in elements:
-#         payload = {
-#             "MonetraTrans": {
-
-#             }
-#         }
