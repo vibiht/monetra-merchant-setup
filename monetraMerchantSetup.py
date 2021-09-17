9245ae46fa19eefe79376b6d6dd60298f829661e
vibiht <89259820+vibiht@users.noreply.github.com>
2021-09-17 11:26:17 -0400

    Fixed POST request and added logging

--- monetraMerchantSetup.py
+++ monetraMerchantSetup.py
@@ -1,15 +1,14 @@
-import numpy
 import pandas
 import configparser
 import requests
 import monetraFunctions
-import socket
-import ssl
-import pprint
+import logging
 
 def main():
+    logging.basicConfig(filename="log.txt",level=logging.INFO,format="%(asctime)s %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p')
     #Open and read monetraMerchanSetup.ini file
     config = configparser.ConfigParser()
+    logging.info("Reading config from monetraMerchantSetup.ini")
     config.read(r"monetraMerchantSetup.ini")
     host=config.get("INPUT","host")
     port=config.getint("INPUT","port")
@@ -17,6 +16,12 @@ def main():
     sheetName=config.get("INPUT","sheetname")
     rowStart=config.getint("INPUT", "rowStart")
     rowCount=config.getint("INPUT", "rowCount")
+    logging.info("Host = " + str(host))
+    logging.info("Port = " + str (port))
+    logging.info("Monetra Excel Path = " + str (excelPath))
+    logging.info("Sheetname = " + str (sheetName))
+    logging.info("Starting Row = " + str (rowStart))
+    logging.info("Number of Rows = " + str (rowCount))
     #Parse Merchant #, Monetra User, iECR, cECR, Settlement Time in "table" object
     table = pandas.read_excel(
         excelPath,
@@ -26,48 +31,43 @@ def main():
         skiprows=rowStart,
         nrows=rowCount
     )
-    #SSL Connection Test
-    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # ssl_sock = ssl.wrap_socket(
-    #     s,
-    #     #ca_certs=r"C:\Users\VTHIRU\Downloads\ca_certs_file",
-    #     cert_reqs=ssl.CERT_REQUIRED
-    # )
-    # ssl_sock.connect(("192.168.1.27",8543))
-    # pprint.pprint(ssl_sock.getpeercert())
-    #ssl_sock.close()
-
-    # Iterate over each row in "table"
-    # for i,row in table.iterrows():
-    #     #Instantiate merchEMV Object using row data
-    #     excelRow = merchEMV(row[1],row[0],row[4],row[2],row[3])        
-    #     #Generate JSON Payload
-    #     payload = (
-    #         monetraFunctions.payloadGenerator(
-    #             monetraFunctions.addMerchantAccountUser(
-    #                 excelRow.user, 
-    #                 excelRow.merchNum,
-    #                 excelRow.interacECR,
-    #                 0
-    #             ),
-    #             monetraFunctions.addMerchantAccountUser(
-    #                 excelRow.user, 
-    #                 excelRow.merchNum,
-    #                 excelRow.creditECR,
-    #                 1
-    #             ),
-    #             monetraFunctions.addMerchantSubUser(
-    #                 excelRow.user
-    #             ),
-    #             monetraFunctions.addMerchantCronTask(
-    #                 excelRow.user,
-    #                 excelRow.settleTime
-    #             )
-    #         )
-    #     )
-    #     #POST
-    #     monReq = requests.post(host,json=payload,verify=False)
-    #     print(monReq.text)
+    #Iterate over each row in "table"
+    for i,row in table.iterrows():
+        logging.info("Reading row #:" + str (rowStart + i+1))
+        #Instantiate merchEMV Object using row data
+        excelRow = monetraFunctions.merchEMV(row[1],row[0],row[4],row[2],row[3]) 
+        logging.info("Row =" + str(excelRow))       
+        #Generate JSON Payload
+        logging.info("Generating JSON Payload")
+        payload = (
+            monetraFunctions.payloadGenerator(
+                monetraFunctions.addMerchantAccountUser(
+                    excelRow.user, 
+                    excelRow.merchNum,
+                    excelRow.interacECR,
+                    0
+                ),
+                monetraFunctions.addMerchantAccountUser(
+                    excelRow.user, 
+                    excelRow.merchNum,
+                    excelRow.creditECR,
+                    1
+                ),
+                monetraFunctions.addMerchantSubUser(
+                    excelRow.user
+                ),
+                monetraFunctions.addMerchantCronTask(
+                    excelRow.user,
+                    excelRow.settleTime
+                )
+            )
+        )
+        logging.info("Payload = " + str(payload))
+        #POST
+        logging.info("Sending POST request to " + host)
+        monReq = requests.post(host,data=payload,verify=False,allow_redirects=True)
+        print(monReq.text)
+        logging.info("Monetra Response: " + monReq.text)
 
 if __name__ == "__main__":
     main()
